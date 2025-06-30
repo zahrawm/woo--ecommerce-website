b4e5aca23f5c8e714204e74853dcd433b3c5d011
Fatimah Zahra Adam <92226557+zahrawm@users.noreply.github.com>
2025-06-30 15:38:24 +0000

    codes updated

--- ecommerce-backend/src/lib/prisma.ts
+++ ecommerce-backend/src/lib/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "../../generated/prisma/client";
 
+
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
